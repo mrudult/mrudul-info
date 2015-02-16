1f014c4d45f767519cf79b90f5e3d0e550380167
Mrudul T <mrudultarwatkar@gmail.com>
2015-02-16 17:57:01 +0530

    site almost done

--- gzip.sh
+++ gzip.sh
@@ -2,3 +2,4 @@
 for file in css/*.css; do gzip -c "$file" > "$file.gz"; done
 for file in js/*.js; do gzip -c "$file" > "$file.gz"; done
 for file in images/*.jpg; do gzip -c "$file" > "$file.gz"; done
+for file in images/*.png; do gzip -c "$file" > "$file.gz"; done
